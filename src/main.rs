284a9c20e53d9667ebc9f819c6972fbd1c8802d3
Asia4046 <kirito234875@gmail.com>
2024-08-05 20:07:21 +0530

    prompt loop

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
 use std::env;
-use std::process::exit;
 use std::fs;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
+use std::process::exit;
 
 fn run_file(path: &str) -> Result<(), String> {
-    match fs::read_to_string(path){
-        Err(msg) => return Err(msg.to_string()), Ok(contents) => run(&contents),
+    match fs::read_to_string(path) {
+        Err(msg) => return Err(msg.to_string()),
+        Ok(contents) => run(&contents),
     }
 }
 
-fn run(_contents: &str) -> Result<(), String>{
+fn run(_contents: &str) -> Result<(), String> {
     return Err("Not Implemented".to_string());
 }
 
 fn run_prompt() -> Result<(), String> {
-    print!("> ");
-    let mut buffer = String::new();
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
+    while true {
+        print!("> ");
+        let mut buffer = String::new();
 
-    match handle.read_line(&mut buffer) {
-        Ok(_) => (),
-        Err(_) => return Err("Couldnt Read Line".to_string()),
+        match io::stdout().flush() {
+            Ok(_) => (),
+            Err(_) => return Err("Could'nt flush stdout".to_string()),
+        }
+
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+
+        match handle.read_line(&mut buffer) {
+            Ok(n) => {
+                if n == 0 {
+                    return Ok(());
+                }
+            },
+            Err(_) => return Err("Couldnt Read Line".to_string()),
+        }
+        println!("You Wrote {}", buffer);
     }
-    println!("You Wrote {}", buffer);
     Ok(())
 }
 
@@ -34,13 +47,13 @@ fn main() {
         println!("Usage: blitz [script]");
         exit(64);
     } else if args.len() == 2 {
-       match run_file(&args[1]){
+        match run_file(&args[1]) {
             Ok(_) => exit(0),
             Err(msg) => {
                 println!("ERROR:\n{}", msg);
                 exit(1);
             }
-       }
+        }
     } else {
         match run_prompt() {
             Ok(_) => exit(0),
