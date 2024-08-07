a9ae737fee2361a52a3dbf44c6f0bc3b0c66885e
Asia4046 <kirito234875@gmail.com>
2024-08-07 21:46:39 +0530

    basic token def

--- src/scanner.rs
+++ src/scanner.rs
@@ -12,24 +12,45 @@ impl Scanner{
 
 #[derive(Debug)]
 pub enum TokenType {
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, START,
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Start,
 
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
+    Bang, BangEqual,
+    Equal, EqualEqual,
+    Greater, GreaterEqual,
+    Less, LessEqual,
 
-    INDENTIFIER, STRING, NUMBER,
+    Identifier, String, Number,
 
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    And, Class, Else, False, Fun, For, If, Null, Or,
+    Print, Return, Super, This, True, Var, While,
 
-    EOF
+    Eof
+}
+
+#[derive(Debug)]
+pub enum LiteralValue{
+    IntValue(i64),
+    FValue(f64),
+    StringValue(String),
+    IdentifierVal(String)
 }
 
 #[derive(Debug)]
 pub struct Token {
     token_type: TokenType,
     lexeme: String,
+    literal: Option<LiteralValue>,
+    line_number: u64,
+}
+
+impl Token{
+    pub fn new(token_type: TokenType, lexeme: String, literal: Option<LiteralValue>, line_number: u64) -> Self {
+        Self {
+            token_type,
+            lexeme,
+            literal, 
+            line_number
+        }
+    }
 }
